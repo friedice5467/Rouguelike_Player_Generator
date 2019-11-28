286426bbf66a67df83651747ef4aff9c29350d61
friedice <58054670+friedice5467@users.noreply.github.com>
2019-11-28 01:03:24 -0800

    Update Human_Generator.py
    #Update#2 I think?
    -replaced age bottom call to real_age and made that into a class instance
    -removed None as an output (due to using print in a while loop)
    -Added a little console pretty-ness

--- Human_Generator.py
+++ Human_Generator.py
@@ -4,7 +4,7 @@ import math
 class Human:
     
     
-    def __init__(self, name, real_hp, real_stamina, real_sex, real_physique, real_iq, real_money, age):
+    def __init__(self, name, real_hp, real_stamina, real_sex, real_physique, real_iq, real_money, real_age):
         self.name = name
         self.real_hp = 10
         self.real_stamina = 10
@@ -12,12 +12,13 @@ class Human:
         self.real_physique = random.randint(1,200)
         self.real_iq = random.randint(1,200)
         self.real_money = random.randint(1,100000)
-        self.age= age
+        self.real_age= random.randint(13,100)
   
     def statcheck(self):
-        return ('{} {} {} {} {} {} {} {}'.format(self.name, self.real_hp, self.real_stamina, self.real_sex, self.real_physique, self.real_iq, self.real_money, self.age)) 
+        return ('{} {} {} {} {} {} {} {}'.format(self.name, self.real_hp, self.real_stamina, self.real_sex, self.real_physique, self.real_iq, self.real_money, self.real_age)) 
     
     def random_stats(self):
+        self.real_age=random.randint(1,100)
         self.real_sex = random.randint(1,2)
         self.real_physique = random.randint(1,200)
         self.real_iq = random.randint(1,200)
@@ -25,53 +26,53 @@ class Human:
     
     def stats_to_text_hp(self):
         if  self.real_hp == 10:
-                self.stat_hp = '[Healthy]'
+                self.stat_hp = 'Healthy'
         elif self.real_hp > int(4):
-                self.stat_hp = '[Average Health]'
+                self.stat_hp = 'Average Health'
         else:
-                self.stat_hp = '[Haggard]'
+                self.stat_hp = 'Haggard'
         return self.stat_hp
 
     def stats_to_text_stamina(self):
         if self.real_stamina == 10:
-                self.stat_stamina = "[Vigorous]"
+                self.stat_stamina = "Vigorous"
         elif self.real_stamina > 4:
-                self.stat_stamina = '[Average Energy]'
+                self.stat_stamina = 'Average Energy'
         else:
-                self.stat_stamina = '[Feeling Exhausted]' 
+                self.stat_stamina = 'Feeling Exhausted' 
         
     def stat_to_gender(self):
         if self.real_sex == 1:
-            self.stat_sex = '[Male]'
+            self.stat_sex = 'Male'
         else:
-            self.stat_sex = '[Female]'
+            self.stat_sex = 'Female'
             
     def stat_how_buff(self):
         if self.real_physique > 140:
-            self.stat_physique = '[Greek God]'
+            self.stat_physique = 'Greek God'
         if self.real_physique  > 80:
-            self.stat_physique = '[Human]'
+            self.stat_physique = 'Human'
         else:
-            self.stat_physique = '[Stick Person]'
+            self.stat_physique = 'Stick Person'
     
     def stat_how_smart(self):
         if self.real_iq > 140:
-            self.stat_iq = '[Brainiac]'
+            self.stat_iq = 'Brainiac'
         if self.real_iq > 80:
-            self.stat_iq = '[Average IQ]'
+            self.stat_iq = 'Average IQ'
         else:
-            self.stat_iq = '[Dumbo]'
+            self.stat_iq = 'Smooth Brain'
 
     def go_to_work(self):
         self.real_money= (self.real_money) +100*(math.log(self.real_iq))
             
     def stat_how_rich(self):
         if self.real_money > 100000:
-            self.stat_money = '[Richer than rich]'
+            self.stat_money = 'Richer than rich'
         if self.real_money > 35000:
-            self.stat_money = '[Middle Income]'
+            self.stat_money = 'Middle Income'
         else:
-            self.stat_money = '[Basically Homeless]'
+            self.stat_money = 'Basically Homeless'
         
     def statcheck_convert(self):
         self.stats_to_text_hp()
@@ -80,26 +81,23 @@ class Human:
         self.stat_how_buff()
         self.stat_how_smart()
         self.stat_how_rich()
-        return ('My name is {}\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n'.format(self.name, self.stat_hp, self.stat_stamina, self.stat_sex, self.stat_physique, self.stat_iq, self.stat_money, self.age))        
+        return ('-----------------------\nMy name is [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{}]\n    [{} years old]\n-----------------------'.format(self.name, self.stat_hp, self.stat_stamina, self.stat_sex, self.stat_physique, self.stat_iq, self.stat_money, self.real_age))        
 
     def satisfaction_yes_no(self):
         yes = {'yes', 'affirmative', 'yeah', 'yea', 'y', 'yup'}
         while True:
-            self.question1 = input('Are you satisfied with your result?\n')
+            self.question1 = input('Are you satisfied with your result?\nYes/No:    ')
             question = self.question1.lower()
             if question in yes:
-                print('result pass')
+                return 'Creation Success'
                 break
             else:
-                human1.random_stats()
-                print(human1.statcheck_convert())
-        print('very cool')
-    
+                self.random_stats()
+                print (self.statcheck_convert())
+                
 
-totalage= random.randint(1,100)
-age = '[{} years old]'.format(totalage)
+
 name= (input('What is your name?\n'))
-human1=Human(name, {}, {}, {}, {}, {}, {}, age)
-print(human1.statcheck())
-print(human1.statcheck_convert())
-print(human1.satisfaction_yes_no())
+human1=Human(name, {}, {}, {}, {}, {}, {}, {})
+print(Human.statcheck_convert(human1))
+print(Human.satisfaction_yes_no(human1))
